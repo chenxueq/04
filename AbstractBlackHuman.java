e89a719a68505129401529b0f1238a1bf8e831cb
chenxueq <91192193+chenxueq@users.noreply.github.com>
2021-09-29 15:43:04 +0800

    Add files via upload

---
+++ AbstractBlackHuman.java
@@ -0,0 +1,14 @@
+package Person;
+
+public abstract  class AbstractBlackHuman implements Human{
+    @Override
+    public void getColor() {
+        System.out.println("我是黑种人");
+    }
+    @Override
+    public void talk() {
+        System.out.println("blackblack");
+    }
+
+
+}
---
+++ AbstractYellowHuman.java
@@ -0,0 +1,12 @@
+package Person;
+
+public abstract  class AbstractYellowHuman implements Human{
+    @Override
+    public void getColor() {
+        System.out.println("我是黄种人");
+    }
+    @Override
+    public void talk() {
+        System.out.println("yellowyellow");
+    }
+}
