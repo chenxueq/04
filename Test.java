168b8af7bcce9694ed3ede1d81ee49fe26287541
chenxueq <91192193+chenxueq@users.noreply.github.com>
2021-09-29 16:04:48 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,15 +1,23 @@
-package Farm;
+package Person;
+
 public class Test {
     public static void main(String[] args) {
-        SGfarm sGfarm=new SGfarm();
-        Animal animal= sGfarm.newAnimal();
-        animal.show();
-        Plant plant=sGfarm.newPlant();
-        plant.show();
-        SRfarm sRfarm=new SRfarm();
-        animal= sRfarm.newAnimal();
-        animal.show();
-        plant=sRfarm.newPlant();
-        plant.show();
+        HumanFactory femaleFactory =new FemaleFactory();
+        HumanFactory maleFactory=new MaleFactory();
+        HumanFactory intersexFactory=new IntersexFactory();
+        femaleFactory.createBlackHuman().getSex();
+        femaleFactory.createWhiteHuman().getSex();
+       Human a=femaleFactory.createYellowHuman();
+       a.getColor();
+       a.talk();
+       a.getSex();
+       Human b=maleFactory.createWhiteHuman();
+       b.getSex();
+       b.getColor();
+       b.talk();
+       Human c=intersexFactory.createYellowHuman();
+       c.getColor();
+       c.talk();
+       c.getSex();
     }
 }
